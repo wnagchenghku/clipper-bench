267a96d261fea01555e8cf71de2eb435ec5e42bf
Cheng Wang2 <nle5255@N-1328.office.hd>
2018-09-24 13:05:04 +0200

    use avx by default

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ input = torch.FloatTensor(1, 3, 224, 224) # allocates
 input.fill_(1) # fills with 1
 
 X = torch.autograd.Variable(input)
-predict_start = datetime.datetime.now() = datetime.datetime.now()
+predict_start = datetime.datetime.now()
 model(X) # runs the model
-predict_end = datetime.datetime.now() = datetime.datetime.now()
+predict_end = datetime.datetime.now()
 
 print("prediction takes %d s, %d us\n" % ((predict_end - predict_start).seconds, (predict_end - predict_start).microseconds))
